b65e1953ab11ed0a4927a2f3de693d739057292c
Fany OCTAVIA <foctavia@e3r7p22.clusters.42paris.fr>
2022-03-20 17:51:17 +0100

    commit C05

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: foctavia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 16:09:14 by foctavia          #+#    #+#             */
+/*   Updated: 2022/03/20 16:41:36 by foctavia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+		return (0);
+	i = 2;
+	while (i <= (nb / 2))
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (ft_is_prime(nb) == 1)
+		return (nb);
+	else
+		return (ft_find_next_prime(nb + 1));
+}
