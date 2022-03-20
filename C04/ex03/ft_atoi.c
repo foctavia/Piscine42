e8d05886760a1193cc6b69917db3491386d37e7d
Fany OCTAVIA <foctavia@e3r8p21.clusters.42paris.fr>
2022-03-20 09:43:35 +0100

    commit gitignore C04

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,79 +6,31 @@
 /*   By: foctavia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 08:49:03 by foctavia          #+#    #+#             */
-/*   Updated: 2022/03/18 12:29:10 by foctavia         ###   ########.fr       */
+/*   Updated: 2022/03/19 10:36:57 by foctavia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_str_is_numeric(char *str)
-{
-	int	i;
-	int	n;
-
-	i = 0;
-	while (str[i] && n != 0)
-	{
-		if (str[i] < '0' || str[i] > '9')
-			n = 0;
-		else
-		{
-			str[i] -= 48;
-			i++;
-		}
-	}
-	return (n);
-}
-
-int	ft_posneg(char *str)
-{
-	int	i;
-	int	n;
-
-	i = 0;
-	n = 1;
-	while ((str[i] == '+' || str[i] == '-') && n != 0)
-	{
-		if ((str[i] == '-' && n != -1) ||
-			(str[i] == '-' && str[i - 1] == '-'))
-			n *= -1;
-		i++;
-	}
-	if (str[i] >= '0' && str[i] <= '9')
-		return (n);
-	else
-		return (0);
-}
-
-int	ft_isspace(char c)
-{
-	if (c == ' ' || c == '\n' || c == '\t'
-		c == '\v' || c == '\f' || c  == '\r')
-		return (1);
-	else
-		return (0);
-}
 
 int	ft_atoi(char *str)
 {
 	int	i;
-	int	j;
-	int	n;
+	int	neg;
+	int	nbr;
 
 	i = 0;
-	n = 1;
-	while (str[i])
+	while (str[i] && ((str[i] >= 9 && str[i] <= 13) || str[i] == ' '))
+		i++;
+	neg = 1;
+	while (str[i] && (str[i] == '+' || str[i] == '-'))
 	{
-		if ((str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
-			str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
-			&& n != 0)
-			n = ft_isspace(str[i]);
-		if ((str[i] == '+' || str[i] == '\n') && n != 0)
-			n = ft_posneg(str[i]);
-		if (str[i] >= '0' && str[i] <= '9' && n != 0)
-			j = n * ft_str_is_numeric(str[i]);
-		else if (str[i] < '0' || str[i] > '9' || n == 0);
-			j = 0;
+		if (str[i] == '-')
+			neg *= -1;
 		i++;
 	}
-	return (j);
+	nbr = 0;
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		nbr = ((nbr * 10) + (str[i] - '0'));
+		i++;
+	}
+	return (nbr * neg);
 }
