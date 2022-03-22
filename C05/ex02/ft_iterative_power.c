a7932db5a3fe938b5c701adaee1432a44dadc99e
Fany OCTAVIA <foctavia@e3r7p9.clusters.42paris.fr>
2022-03-22 21:18:42 +0100

    modify C05

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: foctavia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 19:37:26 by foctavia          #+#    #+#             */
-/*   Updated: 2022/03/19 19:57:49 by foctavia         ###   ########.fr       */
+/*   Updated: 2022/03/21 08:50:47 by foctavia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_iterative_power(int nb, int power)
 	n = nb;
 	if (power < 0)
 		return (0);
-	if (nb == 0 && power == 0)
+	if ((nb == 0 && power == 0) || power == 0)
 		return (1);
 	while (--power > 0)
 		nb *= n;
--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -6,30 +6,22 @@
 /*   By: foctavia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 16:09:14 by foctavia          #+#    #+#             */
-/*   Updated: 2022/03/20 16:41:36 by foctavia         ###   ########.fr       */
+/*   Updated: 2022/03/21 17:23:07 by foctavia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_is_prime(int nb)
+int	ft_find_next_prime(int nb)
 {
 	int	i;
 
 	if (nb <= 1)
-		return (0);
+		return (2);
 	i = 2;
-	while (i <= (nb / 2))
+	while ((nb >= i * i) && (i <= 46340) && (i <= nb / 2))
 	{
 		if (nb % i == 0)
-			return (0);
+			return (ft_find_next_prime(nb + 1));
 		i++;
 	}
-	return (1);
-}
-
-int	ft_find_next_prime(int nb)
-{
-	if (ft_is_prime(nb) == 1)
-		return (nb);
-	else
-		return (ft_find_next_prime(nb + 1));
+	return (nb);
 }
